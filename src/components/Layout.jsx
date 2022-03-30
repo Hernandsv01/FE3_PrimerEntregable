0145d1c2c57c7c7123a9bd35f4a40c46b000efd0
Hernandsv01 <hernanxdsv@gmail.com>
2022-03-30 20:25:38 -0300

    Final funcional

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,34 +1,42 @@
 import { Component } from "react"
 import Text from "./Text"
 import Opcion from "./Opcion"
+import data from "./data"
 
 export default class Layout extends Component{
     constructor(props){
         super(props);
         this.state = {
             idActual: 1,
-            opcion: ""
+            opcionActual: ""
         }
         this.nextId = this.nextId.bind(this);
         this.setOption = this.setOption.bind(this);
     }
     nextId(){
-        this.setState({
-            idActual: (this.state.idActual+1)
-        });
+        if(this.state.idActual+1 <= (data.length+1)/2){
+            this.setState({
+                idActual: (this.state.idActual+1)
+            });
+        }else{
+            //Sweet alert fin
+            alert("Fin");
+        }
     }
     setOption(opcionElegida){
-        this.setState({
-            opcion: opcionElegida
-        });
+        if(this.state.idActual+1 <= (data.length+1)/2){
+            this.setState({
+                opcionActual: opcionElegida
+            });
+        }
     }
     render(){
         return (
             <div className="layout">
-                <Text id={this.state.idActual+this.state.opcion}/>
-                <div className="opciones" id={this.state.idActual} opcion={this.state.opcion}>
-                    <Opcion opcion="A" id={this.state.idActual+this.state.opcion} nextId={this.nextId} setOption={this.setOption}/>
-                    <Opcion opcion="B" id={this.state.idActual+this.state.opcion} nextId={this.nextId} setOption={this.setOption}/>
+                <Text id={this.state.idActual+this.state.opcionActual}/>
+                <div className="opciones" id={this.state.idActual} opcion={this.state.opcionActual}>
+                    <Opcion opcion="A" id={this.state.idActual+this.state.opcionActual} nextId={this.nextId} setOption={this.setOption}/>
+                    <Opcion opcion="B" id={this.state.idActual+this.state.opcionActual} nextId={this.nextId} setOption={this.setOption}/>
                 </div>
                 <div className="recordatorio">
                     {/* <SelAnterior/>
