1ebb4f38c9999a0e545ecd5de014a90af7343863
Hernandsv01 <hernanxdsv@gmail.com>
2022-04-02 21:17:20 -0300

    Proyecto entregable terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
+import { Component } from "react"
 import Layout from "./components/Layout";
+import data from "./components/data"
+import Swal from "sweetalert2";
 
-function App() {
-  return (
-    <div className="App">
-      <Layout/>
-    </div>
-  );
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      isRendered: true,
+      currentId: 1,
+      currentOption: "",
+      optionHistory: []
+    }
+    this.advanceStory = this.advanceStory.bind(this);
+  }
+
+  advanceStory(chosenOption){
+    if(this.state.currentId+1 <= (data.length+1)/2){        
+        this.setState({
+            currentId: (this.state.currentId+1),
+            currentOption: chosenOption,
+            optionHistory: [...this.state.optionHistory, chosenOption.toUpperCase()]
+        });
+    }else{
+      this.setState({isRendered: false});
+    }
+  }
+
+  componentDidUpdate(){
+    if(!this.state.isRendered){
+      Swal.fire({
+        title: "Fin de la historia",
+        text: "Desea volver a comenzar?",
+        denyButtonText: "No",
+        showDenyButton: true,
+        confirmButtonText: "Si",
+        color: "white"
+      }).then(result => {
+          if(result.isConfirmed){
+              this.setState({
+                  currentId: 1,
+                  currentOption: "",
+                  optionHistory: []
+              }, this.setState({isRendered: true}));
+          }else{
+            this.setState({isRendered: true})
+          }
+      });
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.isRendered? <Layout advanceStory={this.advanceStory} currentId={this.state.currentId} currentOption={this.state.currentOption} optionHistory={this.state.optionHistory}/>:""}
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,57 +3,19 @@ import Text from "./Text"
 import Option from "./Option"
 import LastSelected from "./LastSelected"
 import OptionHistory from "./OptionHistory"
-import data from "./data"
-import Swal from "sweetalert2";
 
 export default class Layout extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            currentId: 1,
-            currentOption: "",
-            optionHistory: []
-        }
-        this.advanceStory = this.advanceStory.bind(this);
-    }
-    advanceStory(chosenOption){
-        if(this.state.currentId+1 <= (data.length+1)/2){        
-            this.setState({
-                currentId: (this.state.currentId+1),
-                currentOption: chosenOption,
-                optionHistory: [...this.state.optionHistory, chosenOption.toUpperCase()]
-            });
-        }else{
-            Swal.fire({
-                title: "Fin de la historia",
-                text: "Desea volver a comenzar?",
-                denyButtonText: "No",
-                showDenyButton: true,
-                confirmButtonText: "Si",
-                color: "white",
-            }).then(result => {
-                if(result.isConfirmed){
-                    this.setState({
-                        currentId: 1,
-                        currentOption: "",
-                        optionHistory: []
-                    });
-                }
-            });
-        }
-    }
-
     render(){
         return (
             <div className="layout">
-                <Text id={this.state.currentId+this.state.currentOption}/>
-                <div className="opciones" id={this.state.currentId} opcion={this.state.currentOption}>
-                    <Option opcion="A" id={this.state.currentId+this.state.currentOption} advanceStory={this.advanceStory}/>
-                    <Option opcion="B" id={this.state.currentId+this.state.currentOption} advanceStory={this.advanceStory}/>
+                <Text id={this.props.currentId+this.props.currentOption}/>
+                <div className="opciones" id={this.props.currentId} opcion={this.props.currentOption}>
+                    <Option opcion="A" id={this.props.currentId+this.props.currentOption} advanceStory={this.props.advanceStory}/>
+                    <Option opcion="B" id={this.props.currentId+this.props.currentOption} advanceStory={this.props.advanceStory}/>
                 </div>
                 <div className="recordatorio">
-                    <LastSelected chosen={this.state.optionHistory[this.state.optionHistory.length-1]}/>
-                    <OptionHistory optionHistory={this.state.optionHistory}/>
+                    <LastSelected chosen={this.props.optionHistory[this.props.optionHistory.length-1]}/>
+                    <OptionHistory optionHistory={this.props.optionHistory}/>
                 </div>
             </div>
         );
